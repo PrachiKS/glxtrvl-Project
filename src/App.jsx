3122a417a8981bd0ad21d21f09866e6eeac97c9d
PrachiKS <prachiksonawane25@gmail.com>
2026-03-10 22:10:18 +0530

    changes made in api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
 import './App.css'
+import Contact from './Pages/Contact/Contact'
+import Home from './Pages/Home/Home'
+import Pricing from './Pages/Pricing/Pricing'
+import Training from './Pages/Training/Training'
+
+import { BrowserRouter as Router, Routes, Route}   from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/training' element={<Training/>}/>
+          <Route path='/pricing' element={<Pricing/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
