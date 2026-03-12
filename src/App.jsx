bd8184e843eddcbae95c4884ba9a400c0027ddc9
PrachiKS <prachiksonawane25@gmail.com>
2026-03-12 18:00:18 +0530

    changes made in api

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import './App.css'
-import Contact from './Pages/Contact/Contact'
-import Home from './Pages/Home/Home'
-import Pricing from './Pages/Pricing/Pricing'
-import Training from './Pages/Training/Training'
+import "./App.css";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Pricing from "./Pages/Pricing/Pricing";
+import Training from "./Pages/Training/Training";
 
-import { BrowserRouter as Router, Routes, Route}   from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/training' element={<Training/>}/>
-          <Route path='/pricing' element={<Pricing/>}/>
-          <Route path='/contact' element={<Contact/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/training" element={<Training />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
