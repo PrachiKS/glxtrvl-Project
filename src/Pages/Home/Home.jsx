412ec714c5154b23e6c8c83793f76367b820a1b4
PrachiKS <prachiksonawane25@gmail.com>
2026-03-14 15:03:18 +0530

    home page and Navbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from '../../components/NavBar/Navbar'
+import Hero from './Components/Hero'
 
+import "../Home/Home.css"
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
